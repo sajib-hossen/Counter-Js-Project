021ce7bb56cdbea34dfc53990699175105904663
sajib hossen <sajibhossensa90@gmail.com>
2022-03-26 01:04:19 +0600

    full project added

---
+++ script.js
@@ -0,0 +1,33 @@
+//  start counter
+
+let count = 0;
+
+const value = document.querySelector("#value");
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("Increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+
+    value.textContent = count;
+  });
+});
